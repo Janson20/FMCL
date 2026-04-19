831a41ef5cc5851179c57d0d29461079085fffd6
Janson20 <liujiasheng0720@outlook.com>
2026-04-19 12:22:54 +0800

    chore: release v2.4.0

--- launcher.py
+++ launcher.py
@@ -75,7 +75,7 @@ class MinecraftLauncher:
         self._mcllib = minecraft_launcher_lib
         self.options = minecraft_launcher_lib.utils.generate_test_options()
         self.options["launcherName"] = "FMCL"
-        self.options["launcherVersion"] = "2.3.3"
+        self.options["launcherVersion"] = "2.4.0"
 
         self.current_max = 0
 
